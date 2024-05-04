bee9807c0acb2f05f237af1d3f10b091f98c5f6b
Harold Torres <haroldtm55@gmail.com>
2024-05-05 00:24:04 +1000

    add hierarchy endpoint

--- src/__tests__/api/employees/route.test.ts
+++ src/__tests__/api/employees/route.test.ts
@@ -3,9 +3,56 @@ import { app } from '../../../api/v1/employees/route'
 
 const request = supertest(app)
 
-describe('GET /employees/', () => {
-  it('should return dummy employee data', async () => {
-    const response = await request.get(`/api/v1/employees`)
-    expect(response.body.data).toEqual('returning employeee hierarchy data')
+jest.mock('../../../api/v1/employees/seed', () => ({
+  employeeHandler: {
+    getEmployeesHierarchyAndLevels: jest.fn(),
+  },
+}))
+
+import { employeeHandler } from '../../../api/v1/employees/seed'
+
+describe('GET /hierarchy/', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  it('should return employee hierarchy', async () => {
+    ;(
+      employeeHandler.getEmployeesHierarchyAndLevels as jest.Mock
+    ).mockImplementation(() => {
+      return {
+        employeesHierarchy: [
+          { name: 'Bobby', id: 1, managerId: null, depth: 0 },
+          { name: 'Darren', id: 2, managerId: 1, depth: 1 },
+          { name: 'Harold', id: 3, managerId: 1, depth: 1 },
+        ],
+      }
+    })
+    const response = await request.get(`/api/v1/hierarchy`)
+    expect(response.status).toEqual(200)
+  })
+
+  it('should return 404 if no data is found', async () => {
+    ;(
+      employeeHandler.getEmployeesHierarchyAndLevels as jest.Mock
+    ).mockImplementation(() => {
+      return null
+    })
+    const response = await request.get(`/api/v1/hierarchy`)
+    console.log(response.body)
+
+    expect(response.status).toEqual(404)
+    expect(response.body.error).toEqual('No data found')
+  })
+
+  it('should return 500 if an error occurs', async () => {
+    ;(
+      employeeHandler.getEmployeesHierarchyAndLevels as jest.Mock
+    ).mockImplementation(() => {
+      throw new Error('Test error')
+    })
+
+    const response = await request.get(`/api/v1/hierarchy`)
+    expect(response.status).toEqual(500)
+    expect(response.body.error).toEqual('Internal Server Error')
   })
 })
--- src/api/v1/employees/route.ts
+++ src/api/v1/employees/route.ts
@@ -1,14 +1,19 @@
 import express from 'express'
 import cors from 'cors'
+import { employeeHandler } from './seed'
 
 export const app = express()
 app.use(cors())
 
-app.get('/api/v1/employees', async (req, res) => {
+app.get('/api/v1/hierarchy', async (req, res) => {
   try {
+    const data = employeeHandler.getEmployeesHierarchyAndLevels()
+    if (!data) {
+      return res.status(404).json({ error: 'No data found' })
+    }
     return res
       .status(200)
-      .json({ data: 'returning employeee hierarchy data' })
+      .json({ data: employeeHandler.getEmployeesHierarchyAndLevels() })
   } catch (error) {
     console.error('Error getting employee data:', error)
     return res.status(500).json({ error: 'Internal Server Error' })
